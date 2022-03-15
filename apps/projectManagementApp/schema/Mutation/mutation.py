5c6dbb5f863da65c61587486346cf32d378f663d
SamuelIZABAYO <61514225+SamuelIZABAYO@users.noreply.github.com>
2022-03-15 07:10:40 +0200

    Update mutation.py

--- apps/projectManagementApp/schema/Mutation/mutation.py
+++ apps/projectManagementApp/schema/Mutation/mutation.py
@@ -6,4 +6,3 @@ from apps.projectManagementApp.schema.mutation.create_project_mutation import Cr
 class Mutation(graphene.ObjectType):
     create_project = CreateProject.Field()
     update_project=UpdateProject.Field()
-    
